0e55c7b7d7d841cb6582bd48e6a5f07b410eaa77
Matthew Kirby <matt.kirby@yahoo.com>
2020-10-28 12:02:33 -0400

    finished lab

--- index.js
+++ index.js
@@ -2,5 +2,52 @@ document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
-  // ADD CODE HERE!
+  // DOM ELEMENTS
+  const movesContainer = document.querySelector("#moves-container")
+  const moveButton = document.querySelector("#move-button")
+
+  // EVENT LISTENERS
+  document.addEventListener("keydown", logAcceptableKeyPresses)
+  moveButton.addEventListener("click", moveRobot)
+
+  // delegation version
+  // movesContainer.addEventListener("click", function(event) {
+  //   if (event.target.matches("li")) {
+  //     event.target.remove()
+  //   }
+  //   //can remove the UL
+  //   // event.target.remove()
+  // })
+
+  // EVENT HANDLERS
+  function logAcceptableKeyPresses(event) {
+    if (event.key.startsWith("Arrow")) {
+      const direction = event.key.replace("Arrow", "")
+      addMoves(direction)
+    }
+  }
+
+  function addMoves(direction) {
+    const listItem = document.createElement("li")
+    listItem.textContent = direction
+    movesContainer.append(listItem)
+    //nested version
+    listItem.addEventListener("click", function() {
+      listItem.remove()
+    })
+  }
+
+  function moveRobot() {
+    const intervalID = setInterval(function() {
+      const topLiInContainer = document.querySelector("#moves-container li")
+      if (topLiInContainer) {
+        const direction = topLiInContainer.textContent.toLowerCase()
+        move(direction)
+        topLiInContainer.remove()
+      } else {
+        clearInterval(intervalID)
+        console.log("No Moves Scheduled")
+      }
+    }, 1000)
+  }
 });
